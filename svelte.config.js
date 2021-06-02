1e04691b2d621a4d2c0cf82d2e2cd8e6d3eb8e17
beardage <xypress@gmail.com>
2021-06-02 00:10:25 -0700

    adding tailwindcss

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,12 @@ import preprocess from 'svelte-preprocess';
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: preprocess({
+		postcss: true,
+		defaults: {
+			style: 'postcss',
+		}
+	}),
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
