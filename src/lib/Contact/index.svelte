5f075adf493ac9746218f9527c3214e7045900fe
beardage <xypress@gmail.com>
2021-06-06 13:33:26 -0700

    getting form data set up for working on submission

--- src/lib/Contact/index.svelte
+++ src/lib/Contact/index.svelte
@@ -1,21 +1,29 @@
 <script lang="ts">
 	import { spring } from 'svelte/motion';
 
-	
+	async function handleSubmit(event) {
+		console.log(event);
+		console.log(event.target);
+		console.log(event.target.grid-first-name.value);
+		console.log(event.target.grid-last-name.value);
+		console.log(event.target.email.value);
+		console.log(event.target.message.value);
+	}
+
 </script>
 
-<form class="w-full my-20">
+<form class="w-full my-20" on:submit|preventDefault="{handleSubmit}">
 	<div class="flex flex-wrap -mx-3 mb-6">
 		<div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
 			<label
 				class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-				for="grid-first-name">
+				for="firstname">
 				First Name
 			</label>
 
 			<input
 				class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-				id="grid-first-name"
+				id="firstname"
 				type="text"
 				placeholder="Jane"
 			/>
@@ -26,14 +34,14 @@
 		<div class="w-full md:w-1/2 px-3">
 			<label
 				class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-				for="grid-last-name"
+				for="lastname"
 			>
 				Last Name
 			</label>
 
 			<input
 				class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-				id="grid-last-name"
+				id="lastname"
 				type="text"
 				placeholder="Doe"
 			/>
@@ -44,7 +52,7 @@
 		<div class="w-full px-3">
 			<label
 				class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-				for="grid-password"
+				for="email"
 			>
 				E-mail
 			</label>
@@ -63,7 +71,7 @@
 		<div class="w-full px-3">
 			<label
 				class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-				for="grid-password"
+				for="message"
 			>
 				Message
 			</label>
@@ -83,7 +91,7 @@
 		<div class="md:w-1/3">
 			<button
 				class="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-				type="button">
+				type="submit">
 				Send
 			</button>
 		</div>
