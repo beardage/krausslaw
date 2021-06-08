36768b62846d00716650ade6d2121d00c69e1d78
beardage <xypress@gmail.com>
2021-06-07 21:46:00 -0700

    installing netlify adapter for deploying

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from 'svelte-preprocess';
+const netlify = require('@sveltejs/adapter/netlify');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,11 +8,12 @@ const config = {
 	preprocess: preprocess({
 		postcss: true,
 		defaults: {
-			style: 'postcss',
+			style: 'postcss'
 		}
 	}),
 
 	kit: {
+		adapter: netlify(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
