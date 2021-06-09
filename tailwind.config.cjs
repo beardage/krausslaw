b0efd24f85863f8a9bcb67c722b1b31698e6e0a2
beardage <xypress@gmail.com>
2021-06-08 22:48:06 -0700

    removing boilerplate styles and starting to style navigation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,20 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['src/app.html', 'src/**/*.svelte'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      gray: colors.coolGray,
+      amber: colors.amber,
+      primary: colors.amber,
+      secondary: colors.emerald
+    },
+    container: {
+      padding: '2rem',
+    },
     extend: {},
   },
   variants: {
