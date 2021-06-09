b0804f5f6d694908f0cbd83e1f11b60ba80caaa0
beardage <xypress@gmail.com>
2021-06-08 21:58:04 -0700

    prototyping footer content

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -10,7 +10,23 @@
 </main>
 
 <footer>
-	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	<div class="container mx-auto grid grid-cols-3 gap-4">
+		<div>
+			<a class="block" href="/about/">About</a>
+			<a class="block" href="/faq/">Frequently Asked Questions</a>
+			<a class="block" href="/contact/">Contact</a>
+		</div>
+		<div>
+			<p>Test address<br>
+			1234 N. test address drive<br>
+			tucson, arizona 12345</p>
+		</div>
+		<div>
+			<a class="block" href="#">Facebook</a>
+			<a class="block" href="#">Instagram</a>
+			<a class="block" href="#">Linkedin</a>
+		</div>
+	</div>
 </footer>
 
 <style>
