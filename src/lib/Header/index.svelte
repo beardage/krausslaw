a1e61f0fe3d5138bc2e144dfc080a1eb243cd4e3
beardage <xypress@gmail.com>
2021-06-09 20:05:15 -0700

    correcting header link to point at faq

--- src/lib/Header/index.svelte
+++ src/lib/Header/index.svelte
@@ -10,7 +10,7 @@
 		<ul class="flex justify-start uppercase">
 			<li class="inline-block" class:active={$page.path === '/'}><a class="p-4" sveltekit:prefetch href="/">Home</a></li>
 			<li class="inline-block" class:active={$page.path === '/about'}><a class="p-4" sveltekit:prefetch href="/about">About</a></li>
-			<li class="inline-block" class:active={$page.path === '/todos'}><a class="p-4" sveltekit:prefetch href="/todos">Todos</a></li>
+			<li class="inline-block" class:active={$page.path === '/faq'}><a class="p-4" sveltekit:prefetch href="/todos">Frequently Asked Questions</a></li>
 			<li class="inline-block" class:active={$page.path === '/contact'}><a class="p-4" sveltekit:prefetch href="/contact">Contact</a></li>
 		</ul>
 	</nav>
