2deb32f2018684acf184dd3a147677452ea43e3c
beardage <xypress@gmail.com>
2021-06-14 21:29:33 -0700

    changing the column structure of the footer and adding hoverstate

--- src/lib/Footer/index.svelte
+++ src/lib/Footer/index.svelte
@@ -1,20 +1,27 @@
 <footer>
-	<div class="container grid grid-cols-3 gap-4 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+	<div class="container grid grid-cols-4 gap-4 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-sm">
 		<div>
-			<a class="text-gray-300 block" href="/about/">About</a>
-			<a class="text-gray-300 block" href="/faq/">Frequently Asked Questions</a>
-			<a class="text-gray-300 block" href="/contact/">Contact</a>
+			<a class="text-gray-300 hover:text-gray-100 block" href="/">Home</a>
+			<a class="text-gray-300 hover:text-gray-100 block" href="/about/">About</a>
+			<a class="text-gray-300 hover:text-gray-100 block" href="/faq/">Frequently Asked Questions</a>
+			<a class="text-gray-300 hover:text-gray-100 block" href="/contact/">Contact</a>
 		</div>
 		<div> 
 			<p class="text-gray-300">test address<br>
 			1234 N. test address drive<br>
-			tucson, arizona 12345</p>
+			tucson, arizona 12345<br>
+			p: <a href="tel:123.123.1234">123.123.1234</a><br>
+			f: <a href="fax:321.321.4321">321.321.4321</a></p>
 		</div>
+		<div></div>
 		<div> 
-			<a class="text-gray-300 block" href="#">Facebook</a> 
-			<a class="text-gray-300 block" href="#">Instagram</a>
-			<a class="text-gray-300 block" href="#">Linkedin</a> 
+			<a class="text-gray-300 hover:text-gray-100 block" href="#">Facebook</a> 
+			<a class="text-gray-300 hover:text-gray-100 block" href="#">Instagram</a>
+			<a class="text-gray-300 hover:text-gray-100 block" href="#">Linkedin</a> 
+			<br>
+			<br>
+			<p class="text-gray-300">©2021 KraussLaw. All Rights Reserved.</p>
 		</div>
-	</div>    
+	</div>
 </footer> 
 
