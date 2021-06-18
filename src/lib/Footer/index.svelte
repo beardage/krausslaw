3c55d25a4723d06aac9a91a4795f96568f390e9f
beardage <xypress@gmail.com>
2021-06-17 23:01:44 -0700

    improving the footer styles

--- src/lib/Footer/index.svelte
+++ src/lib/Footer/index.svelte
@@ -1,27 +1,26 @@
-<footer>
-	<div class="container grid grid-cols-4 gap-4 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-sm">
-		<div class="uppercase">
-			<a class="text-secondary-800 block" href="/">Home</a>
-			<a class="text-secondary-800 block" href="/about/">About</a>
-			<a class="text-secondary-800 block" href="/practice-areas/">Practice Areas</a>
-			<a class="text-secondary-800 block" href="/faq/">FAQs</a>
-			<a class="text-secondary-800 block" href="/contact/">Contact</a>
+<footer class="border-t border-primary-900 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+	<div class="container grid grid-cols-4 gap-4 text-sm pt-8 pb-8">
+		<div class="uppercase leading-6">
+			<a class="text-secondary-800 font-semibold block" href="/">Home</a>
+			<a class="text-secondary-800 font-semibold block" href="/about/">About</a>
+			<a class="text-secondary-800 font-semibold block" href="/practice-areas/">Practice Areas</a>
+			<a class="text-secondary-800 font-semibold block" href="/faq/">FAQs</a>
+			<a class="text-secondary-800 font-semibold block" href="/contact/">Contact</a>
 		</div>
-		<div> 
-			<p class="text-secondary-800">test address<br>
+		<div class="uppercase leading-6"> 
+			<p class="text-secondary-800 font-semibold">test address<br>
 			1234 N. test address drive<br>
 			tucson, arizona 12345<br>
 			p: <a href="tel:123.123.1234">123.123.1234</a><br>
 			f: <a href="fax:321.321.4321">321.321.4321</a></p>
 		</div>
 		<div></div>
-		<div> 
-			<a class="text-secondary-800 block" href="#">Facebook</a> 
-			<a class="text-secondary-800 block" href="#">Instagram</a>
-			<a class="text-secondary-800 block" href="#">Linkedin</a> 
+		<div class="uppercase leading-6"> 
+			<a class="text-secondary-800 font-semibold block" href="#">Facebook</a> 
+			<a class="text-secondary-800 font-semibold block" href="#">Instagram</a>
+			<a class="text-secondary-800 font-semibold block" href="#">Linkedin</a> 
 			<br>
-			<br>
-			<p class="text-gray-300">©2021 KraussLaw. All Rights Reserved.</p>
+			<p class="text-secondary-800">©2021 KraussLaw. All Rights Reserved.</p>
 		</div>
 	</div>
 </footer> 
