b687abba73c10151b0e1638766d9bced936ebe4b
beardage <xypress@gmail.com>
2021-06-22 22:55:11 -0700

    reorganizing faq components and creating subcomponents

---
+++ src/lib/FAQs/Faq.svelte
@@ -0,0 +1,8 @@
+<script>
+	export let faq;
+</script>
+
+<div class="faq">
+	<p>{faq.question}</p>
+	<p>{faq.answer}</p>
+</div>
--- src/lib/FAQs/index.svelte
+++
@@ -1,5 +0,0 @@
-<div class="block bg-secondary-800">
-	<h3>faqs</h3>
-
-	test
-</div>
