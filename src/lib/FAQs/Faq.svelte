407862c6ca6ab64b25817c4fb68c2c75538b456a
beardage <xypress@gmail.com>
2021-06-24 22:40:39 -0700

    added toggling to JS elements

--- src/lib/FAQs/Faq.svelte
+++ src/lib/FAQs/Faq.svelte
@@ -1,12 +1,19 @@
 <script>
 	export let faq;
+	
+	let active = false;
+	let activeAnswerStyle = 'block';	
+	
+	function toggleActive() {
+		active = !active;	
+	}
 </script>
 
-<div class="faq border mb-4">
-	<div class="question text-gray-100 bg-secondary-800 p-4 cursor-pointer">
+<div class:active={active} class="faq border mb-4">
+	<div on:click={toggleActive} class="question text-gray-100 bg-secondary-800 p-4 cursor-pointer">
 		<p>{faq.question}</p>
 	</div>
-	<div class="answer p-4">
+	<div class:hidden={!active} class="answer p-4">
 		<p>{faq.answer}</p>
 	</div>
 </div>
